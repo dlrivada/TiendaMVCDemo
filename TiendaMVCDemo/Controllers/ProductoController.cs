338041f97b8fbc99eea1919e6f7f97e036e58810
David Lozano Rivada <dlrivada@hotmail.com>
2015-11-11 14:10:19 +0100

    11/11/2015

---
+++ TiendaMVCDemo/Controllers/ProductoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Mvc;
+using TiendaMVCDemo.Models;
+
+namespace TiendaMVCDemo.Controllers
+{
+    public class ProductoController : Controller , IDisposable
+    {
+        readonly Tienda05Entities _db = new Tienda05Entities();
+
+        public ActionResult Index()
+        {
+            var data = _db.Producto;
+
+            return View(data);
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            var data = _db.Producto.Find(id);
+            return View(data);
+        }
+
+        // GET: Producto
+        public ActionResult Alta()
+        {
+            return View(new Producto());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Producto model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Producto.Add(model);
+                _db.SaveChanges();
+                return View(new Producto());
+            }
+            return View(model);
+        }
+
+        void IDisposable.Dispose() 
+        {
+            _db?.Dispose();
+        }
+    }
+}
